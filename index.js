fb91394b3a675c694fc2e2d8f16f4511a4f26fbb
Jaime Iniesta <jaimeiniesta@gmail.com>
2017-04-29 21:51:36 +0200

    adds an option to output JSON directly

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ program.version(version)
 .option('-t, --tags <list>', 'Tags of rules to run, comma separated', utils.splitList)
 .option('-b, --browser [browser-name]', 'Which browser to run (Webdriver required)')
 .option('-s, --save [filename]', 'Save the output as a JSON file. Filename is optional')
+.option('-j, --json-dump', 'Output the results directly as JSON, without saving to a file')
 .option('-d, --dir <path>', 'Output directory')
 .option('-a, --axe-source', 'Path to axe.js file')
 // .option('-c, --config <file>', 'Path to custom axe configuration')
@@ -30,7 +31,9 @@ program.axeSource = utils.getAxeSource(program.axeSource);
 const axeVersion = utils.getAxeVersion(program.axeSource)
 
 // Setup axe with the appropriate config
-console.log(colors.bold('Running axe-core ' + axeVersion + ' in ' + program.browser))
+if (!program.jsonDump) {
+	console.log(colors.bold('Running axe-core ' + axeVersion + ' in ' + program.browser))
+}
 
 // Make valid URLs of all pages
 const urls = program.args.map(utils.parseUrl);
@@ -48,70 +51,80 @@ axeTestUrls(urls, program, {
 	 * Inform the user what page is tested
 	 */
 	onTestStart: function (url) {
-		console.log(
-			colors.bold('\nTesting ' + link(url)) +
-			' ... please wait, this may take a minute.'
-		);
+		if (!program.jsonDump) {
+			console.log(
+				colors.bold('\nTesting ' + link(url)) +
+				' ... please wait, this may take a minute.'
+			);
+		}
 	},
 
 	/**
 	 * Put the result in the console
 	 */
 	onTestComplete: function logResults(results) {
-		const violations = results.violations
-		if (violations.length === 0) {
-			console.log(colors.green('  0 violations found!'))
-			return;
+		if (!program.jsonDump) {
+			const violations = results.violations
+			if (violations.length === 0) {
+				console.log(colors.green('  0 violations found!'))
+				return;
+			}
+
+			const issueCount = violations.reduce((count, violation) => {
+				console.log('\n' + error(
+					'  Violation of %j with %d occurrences!\n') +
+					'    %s. Correct invalid elements at:\n' +
+					(violation.nodes.map( node =>
+					'     - ' + node.target + '\n'
+					).join('')) +
+					'    For details, see: %s',
+					violation.id,
+					violation.nodes.length,
+					violation.description,
+					link(violation.helpUrl.split('?')[0])
+				);
+				return count + violation.nodes.length
+			}, 0);
+
+			console.log(error('\n%d Accessibility issues detected.'), issueCount)
 		}
-
-		const issueCount = violations.reduce((count, violation) => {
-			console.log('\n' + error(
-				'  Violation of %j with %d occurrences!\n') +
-				'    %s. Correct invalid elements at:\n' +
-				(violation.nodes.map( node =>
-				'     - ' + node.target + '\n'
-				).join('')) +
-				'    For details, see: %s',
-				violation.id,
-				violation.nodes.length,
-				violation.description,
-				link(violation.helpUrl.split('?')[0])
-			);
-			return count + violation.nodes.length
-		}, 0);
-
-		console.log(error('\n%d Accessibility issues detected.'), issueCount)
 	}
 }).then(function (outcome) {
-	// All results are in, quit the browser, and give a final report
-	if (outcome.length > 1) {
-		console.log(colors.bold.underline(
-			'Testing complete of %d pages'),
-			outcome.length
-		);
-	}
-	// Save the outcome
-	if (program.save || program.dir) {
-		return saveOutcome(outcome, program.save, program.dir)
-		.then(fileName => {
-			console.log('\nSaved file at', fileName)
-		}).catch(err => {
-			console.log(error('\nUnable to save file!\n') + err);
+  if (program.jsonDump) {
+    console.log(JSON.stringify(outcome, null, '  '))
+  } else {
+		// All results are in, quit the browser, and give a final report
+		if (outcome.length > 1) {
+			console.log(colors.bold.underline(
+				'Testing complete of %d pages'),
+				outcome.length
+			);
+		}
+		// Save the outcome
+		if (program.save || program.dir) {
+			return saveOutcome(outcome, program.save, program.dir)
+			.then(fileName => {
+				console.log('\nSaved file at', fileName)
+			}).catch(err => {
+				console.log(error('\nUnable to save file!\n') + err);
+				return Promise.resolve();
+			})
+		} else {
 			return Promise.resolve();
-		})
-	} else {
-		return Promise.resolve();
+		}
+	}
+}).then(() => {
+	if (!program.jsonDump) {
+		// Give a notification that 0 issues in axe doesn't mean perfect a11y
+		console.log(colors.italic('\n' +
+			'Please note that only 20% to 50% of all accessibility ' +
+			'issues can automatically be detected. \nManual testing is ' +
+			'always required. For more information see:\n%s\n'
+		), link(
+			'https://dequeuniversity.com/curriculum/courses/testing'
+		));
 	}
 
-}).then(() => {
-	// Give a notification that 0 issues in axe doesn't mean perfect a11y
-	console.log(colors.italic('\n' +
-		'Please note that only 20% to 50% of all accessibility ' +
-		'issues can automatically be detected. \nManual testing is ' +
-		'always required. For more information see:\n%s\n'
-	), link(
-		'https://dequeuniversity.com/curriculum/courses/testing'
-	));
 }).catch((e) => {
 	// On error, report it and quit the browser
 	console.log(
