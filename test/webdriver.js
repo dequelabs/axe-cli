d8911ec4ff975e1121a7ba8b83b625bd82bc560a
Wilco Fiers <wilco.fiers@deque.com>
2017-05-29 13:28:21 +0200

    feat: Add unit tests

---
+++ test/webdriver.js
@@ -0,0 +1,40 @@
+/* global mocha */
+const { assert } = require('chai')
+const { startDriver, stopDriver } = require('../lib/webdriver')
+
+describe('startDriver', () => {
+  it('returns a promise', () => {
+    const out = startDriver({ 'browser': 'phantomjs' })
+
+    assert.isFunction(out.then)
+    assert.isFunction(out.catch)
+  })
+
+  it('creates a driver', (done) => {
+    startDriver({ 'browser': 'phantomjs' })
+    .then((config) => {
+      assert.isObject(config.driver)
+      assert.isFunction(config.driver.manage)
+
+      stopDriver(config)
+      done()
+    })
+  })
+
+  it('adds phantom if phantom is set', (done) => {
+    startDriver({ 'browser': 'phantomjs' })
+    .then((config) => {
+      assert.isObject(config.phantom)
+      stopDriver(config)
+      done()
+    })
+  })
+})
+
+describe('stopDriver', () => {
+  it('calls phantom.kill if phantom is set', () => {
+    let called = 0;
+    stopDriver({ phantom: { kill: () => called++ } })
+    assert.equal(called, 1)
+  })
+})
