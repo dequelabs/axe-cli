1d87607f8028d7ddee363be5a29f004aa064b76e
Wilco Fiers <wilco.fiers@deque.com>
2017-07-17 11:25:41 +0200

    chore: Some cleanup work

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const program = require('commander');
-
 const version = require('./package.json').version;
 const axeTestUrls = require('./lib/axe-test-urls');
 const saveOutcome = require('./lib/save-outcome');
 const utils = require('./lib/utils');
 const CliReporter = require('./lib/cli-reporter');
-const noop = function () {};
 
 program.version(version)
 .usage('<url...> [options]')
@@ -22,6 +20,7 @@ program.version(version)
 .option('-q, --exit', 'Exit with `1` failure code if any a11y tests fail')
 .option('-p, --reporter [name]', 'Which format to use for the output (default cli)', 'cli')
 // .option('-c, --config <file>', 'Path to custom axe configuration')
+.option('--no-color', 'Turn off colored output')
 .option('--timeout <n>', 'Set how much time (second) axe has to run (default: 90)', 90)
 .option('--timer', 'Log the time it takes to run')
 .option('--debug', 'Display the full error stack')
@@ -32,16 +31,16 @@ program.axeSource = utils.getAxeSource(program.axeSource);
 
 // Try to match the version of axe that's used
 const axeVersion = utils.getAxeVersion(program.axeSource)
-const reporter = new CliReporter(program);
 
 // Setup axe with the appropriate config
+const reporter = new CliReporter(program);
 reporter.testStart(axeVersion, program.browser);
 
 // Make valid URLs of all pages
 const urls = program.args.map(utils.parseUrl);
 
 if (urls.length === 0) {
-	reporter.error('No url was specified. Check `axe -h` for help\n');
+	reporter.error('\nNo url was specified. Check `axe -h` for help\n');
 	process.exitCode = 1;
 	return;
 }
@@ -72,28 +71,23 @@ axeTestUrls(urls, program, {
 	reporter.testComplete(outcome.length);
 
 	if (program.timer) {
-		console.timeEnd('Total test time');
 		reporter.newLine();
+		console.timeEnd('Total test time');
 	}
 
 	// Save the outcome
 	if (program.save || program.dir) {
 		return saveOutcome(outcome, program.save, program.dir)
 		.then(fileName => {
-			reporter.status('Saved file at', fileName, '\n')
+			reporter.status('\nSaved file at', fileName)
 
 		}).catch(err => {
-			reporter.error('Error: Unable to save file!');
+			reporter.error('\nError: Unable to save file!');
 			reporter.errorDetails(err);
-			reporter.newLine();
 
 			process.exitCode = 1;
-			return Promise.resolve();
-		})
-	} else {
-		return Promise.resolve();
+		});
 	}
-
 }).then(() => {
 	reporter.axeDisclaimer();
 
