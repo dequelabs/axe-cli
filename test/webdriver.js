f95caa5a56d012ca5497c4002d2f699ba2fb2892
Wilco Fiers <wilco.fiers@deque.com>
2017-09-18 21:57:36 +0200

    feat: Use chrome-headless as default browser

--- test/webdriver.js
+++ test/webdriver.js
@@ -2,28 +2,85 @@
 'use strict';
 
 const assert = require('chai').assert
-const startDriver = require('../lib/webdriver').startDriver
-const stopDriver = require('../lib/webdriver').stopDriver
+const {startDriver, stopDriver} = require('../lib/webdriver')
+const chromedriver = require('chromedriver')
+const chrome = require('selenium-webdriver/chrome')
 
 describe('startDriver', () => {
-	it('returns a promise', (done) => {
-		const out = startDriver({ 'browser': 'chrome' })
-
-		assert.isFunction(out.then)
-		assert.isFunction(out.catch)
-		out.then(stopDriver)
-			.then(done)
+	let config, browser
+	beforeEach(() => {
+		browser = 'chrome-headless';
+		config = {
+			get browser() { return browser }
+		}
 	})
 
-	it('creates a driver', (done) => {
-		startDriver({ 'browser': 'chrome' })
-		.then((config) => {
-			assert.isObject(config.driver)
-			assert.isFunction(config.driver.manage)
-
-			stopDriver(config)
+	afterEach(done => {
+		stopDriver(config)
+		const service = chrome.getDefaultService()
+		if (service.isRunning()) {
+			service.stop().then(() => {
+				// An unfortunately hacky way to clean up
+				// the service. Stop will shut it down,
+				// but it doesn't reset the local state
+				service.address_ = null;
+				chrome.setDefaultService(null);
+				done()
+			})
+		} else {
 			done()
-		})
+		}
+	})
+
+	it('creates a driver', done => {
+		startDriver(config)
+			.then((config) => {
+				assert.isObject(config.driver)
+				assert.isFunction(config.driver.manage)
+			})
+			.then(done, done)
+	})
+
+	it('sets the config.browser as the browser', done => {
+		browser = 'chrome'
+		startDriver(config)
+			.then(config => config.driver.getCapabilities())
+			.then(capabilities => {
+				assert.equal(capabilities.get('browserName'), browser)
+			})
+			.then(done, done)
+	})
+
+	it('sets the browser as chrome with chrome-headless', done => {
+		browser = 'chrome-headless'
+		startDriver(config)
+			.then(config => config.driver.getCapabilities())
+			.then(capabilities => {
+				assert.equal(capabilities.get('browserName'), 'chrome')
+			})
+			.then(done, done)
+	})
+
+	it('uses the chromedriver path with chrome-headless', done => {
+		browser = 'chrome-headless'
+		startDriver(config)
+			.then(config => {
+				const service = chrome.getDefaultService()
+				assert.equal(service.executable_, chromedriver.path)
+			})
+			.then(done, done)
+	})
+
+	it('sets the --headless flag with chrome-headless', done => {
+		browser = 'chrome-headless'
+		startDriver(config)
+			.then(({ builder }) => builder.getCapabilities())
+			.then(capabilities => {
+				const chromeOptions = capabilities.get('chromeOptions');
+				assert.isObject(chromeOptions)
+				assert.deepEqual(chromeOptions.args, ['--headless'])
+			})
+			.then(done, done)
 	})
 })
 
@@ -31,9 +88,9 @@ describe('stopDriver', () => {
 	it('calls browser.quit', () => {
 		let called = 0;
 		stopDriver({
-			browser: 'chrome',
-			driver: { quit: () => called++ }
-		})
+				browser: 'chrome-headless',
+				driver: { quit: () => called++ }
+			})
 		assert.equal(called, 1)
 	})
 })
