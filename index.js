01aecc3eb416af8864a28b01ac7b93e8834d93fc
Stephen Mathieson <me@stephenmathieson.com>
2018-10-22 10:47:39 -0400

    chore: add ESLint and Prettier
    This patch introduces ESLint and styles all code with Prettier. I've configured Prettier to match the existing code-style as much as possible.

--- index.js
+++ index.js
@@ -6,65 +6,98 @@ const link = colors.underline.blue;
 const error = colors.red.bold;
 
 const version = require('./package.json').version;
-const axeTestUrls = require('./lib/axe-test-urls')
-const saveOutcome = require('./lib/save-outcome')
-const utils = require('./lib/utils')
+const axeTestUrls = require('./lib/axe-test-urls');
+const saveOutcome = require('./lib/save-outcome');
+const utils = require('./lib/utils');
 
-program.version(version)
-.usage('<url...> [options]')
-.option('-i, --include <list>', 'CSS selector of included elements, comma separated', utils.splitList)
-.option('-e, --exclude <list>', 'CSS selector of included elements, comma separated', utils.splitList)
-.option('-r, --rules <list>', 'IDs of rules to run, comma separated', utils.splitList)
-.option('-t, --tags <list>', 'Tags of rules to run, comma separated', utils.splitList)
-.option('-l, --disable <list>', 'IDs of rules to disable, comma separated', utils.splitList)
-.option('-b, --browser [browser-name]', 'Which browser to run (Webdriver required)')
-.option('-s, --save [filename]', 'Save the output as a JSON file. Filename is optional')
-.option('-d, --dir <path>', 'Output directory')
-.option('-a, --axe-source <path>', 'Path to axe.js file')
-.option('-q, --exit', 'Exit with `1` failure code if any a11y tests fail')
-.option('--load-delay <n>', 'Set how much time (milliseconds) axe will wait after page load before running the audit (default: 0)', 0)
-.option('--timeout <n>', 'Set how much time (seconds) axe has to run (default: 90)', 90)
-.option('--timer', 'Log the time it takes to run')
-.option('--show-errors', 'Display the full error stack')
-// TODO: Replace this with a reporter option, this required adding
-// a reporter option to axe-webdriverjs
-.option('--no-reporter', 'Turn the CLI reporter off')
+program
+	.version(version)
+	.usage('<url...> [options]')
+	.option(
+		'-i, --include <list>',
+		'CSS selector of included elements, comma separated',
+		utils.splitList
+	)
+	.option(
+		'-e, --exclude <list>',
+		'CSS selector of included elements, comma separated',
+		utils.splitList
+	)
+	.option(
+		'-r, --rules <list>',
+		'IDs of rules to run, comma separated',
+		utils.splitList
+	)
+	.option(
+		'-t, --tags <list>',
+		'Tags of rules to run, comma separated',
+		utils.splitList
+	)
+	.option(
+		'-l, --disable <list>',
+		'IDs of rules to disable, comma separated',
+		utils.splitList
+	)
+	.option(
+		'-b, --browser [browser-name]',
+		'Which browser to run (Webdriver required)'
+	)
+	.option(
+		'-s, --save [filename]',
+		'Save the output as a JSON file. Filename is optional'
+	)
+	.option('-d, --dir <path>', 'Output directory')
+	.option('-a, --axe-source <path>', 'Path to axe.js file')
+	.option('-q, --exit', 'Exit with `1` failure code if any a11y tests fail')
+	.option(
+		'--load-delay <n>',
+		'Set how much time (milliseconds) axe will wait after page load before running the audit (default: 0)',
+		0
+	)
+	.option(
+		'--timeout <n>',
+		'Set how much time (seconds) axe has to run (default: 90)',
+		90
+	)
+	.option('--timer', 'Log the time it takes to run')
+	.option('--show-errors', 'Display the full error stack')
+	// TODO: Replace this with a reporter option, this required adding
+	// a reporter option to axe-webdriverjs
+	.option('--no-reporter', 'Turn the CLI reporter off')
 
-// .option('-c, --config <file>', 'Path to custom axe configuration')
-.parse(process.argv);
+	// .option('-c, --config <file>', 'Path to custom axe configuration')
+	.parse(process.argv);
 
-program.browser = utils.parseBrowser(program.browser)
+program.browser = utils.parseBrowser(program.browser);
 program.axeSource = utils.getAxeSource(program.axeSource);
 
 if (!program.axeSource) {
-	console.log(error(
-		'Unable to find the axe-core source file.'
-	));
-	return 
+	console.log(error('Unable to find the axe-core source file.'));
+	return;
 }
 
 let cliReporter;
 if (program.reporter === false) {
-	cliReporter = function () {};
+	cliReporter = function() {};
 } else {
-	cliReporter = function (...args) {
-		console.log(...args)
+	cliReporter = function(...args) {
+		console.log(...args);
 	};
 }
 
 // Try to match the version of axe that's used
-const axeVersion = utils.getAxeVersion(program.axeSource)
+const axeVersion = utils.getAxeVersion(program.axeSource);
 
 // Setup axe with the appropriate config
-console.log(colors.bold('Running axe-core ' + axeVersion + ' in ' + program.browser))
+console.log(
+	colors.bold('Running axe-core ' + axeVersion + ' in ' + program.browser)
+);
 
 // Make valid URLs of all pages
 const urls = program.args.map(utils.parseUrl);
 
 if (urls.length === 0) {
-	console.log(error(
-		'No url was specified. Check `axe -h` for help\n'
-	))
+	console.log(error('No url was specified. Check `axe -h` for help\n'));
 	process.exitCode = 1;
 	return;
 }
@@ -74,10 +107,10 @@ axeTestUrls(urls, program, {
 	/**
 	 * Inform the user what page is tested
 	 */
-	onTestStart: function (url) {
+	onTestStart: function(url) {
 		console.log(
 			colors.bold('\nTesting ' + link(url)) +
-			' ... please wait, this may take a minute.'
+				' ... please wait, this may take a minute.'
 		);
 		if (program.timer) {
 			console.time('Total test time');
@@ -88,86 +121,89 @@ axeTestUrls(urls, program, {
 	 * Put the result in the console
 	 */
 	onTestComplete: function logResults(results) {
-		const violations = results.violations
+		const violations = results.violations;
 		if (violations.length === 0) {
-			cliReporter(colors.green('  0 violations found!'))
+			cliReporter(colors.green('  0 violations found!'));
 			return;
 		}
 
 		const issueCount = violations.reduce((count, violation) => {
-			cliReporter('\n' + error(
-				'  Violation of %j with %d occurrences!\n') +
-				'    %s. Correct invalid elements at:\n' +
-				(violation.nodes.map( node =>
-				'     - ' + utils.selectorToString(node.target) + '\n'
-				).join('')) +
-				'    For details, see: %s',
+			cliReporter(
+				'\n' +
+					error('  Violation of %j with %d occurrences!\n') +
+					'    %s. Correct invalid elements at:\n' +
+					violation.nodes
+						.map(node => '     - ' + utils.selectorToString(node.target) + '\n')
+						.join('') +
+					'    For details, see: %s',
 				violation.id,
 				violation.nodes.length,
 				violation.description,
 				link(violation.helpUrl.split('?')[0])
 			);
-			return count + violation.nodes.length
+			return count + violation.nodes.length;
 		}, 0);
 
-		cliReporter(error('\n%d Accessibility issues detected.'), issueCount)
+		cliReporter(error('\n%d Accessibility issues detected.'), issueCount);
 
 		if (program.exit) {
 			process.exitCode = 1;
 		}
 	}
-}).then(function (outcome) {
-	console.log('');
-	if (program.timer) {
-		console.timeEnd('Total test time');
-	}
-	// All results are in, quit the browser, and give a final report
-	if (outcome.length > 1) {
-		console.log(colors.bold.underline(
-			'Testing complete of %d pages\n'),
-			outcome.length
-		);
-	} else if (program.timer) {
+})
+	.then(function(outcome) {
 		console.log('');
-	}
+		if (program.timer) {
+			console.timeEnd('Total test time');
+		}
+		// All results are in, quit the browser, and give a final report
+		if (outcome.length > 1) {
+			console.log(
+				colors.bold.underline('Testing complete of %d pages\n'),
+				outcome.length
+			);
+		} else if (program.timer) {
+			console.log('');
+		}
 
-	// Save the outcome
-	if (program.save || program.dir) {
-		return saveOutcome(outcome, program.save, program.dir)
-		.then(fileName => {
-			console.log('Saved file at', fileName, '\n')
-		}).catch(err => {
-			console.log(error('Unable to save file!\n') + err);
-			process.exitCode = 1;
+		// Save the outcome
+		if (program.save || program.dir) {
+			return saveOutcome(outcome, program.save, program.dir)
+				.then(fileName => {
+					console.log('Saved file at', fileName, '\n');
+				})
+				.catch(err => {
+					console.log(error('Unable to save file!\n') + err);
+					process.exitCode = 1;
+					return Promise.resolve();
+				});
+		} else {
 			return Promise.resolve();
-		})
-	} else {
-		return Promise.resolve();
-	}
-
-}).then(() => {
-
-	// Give a notification that 0 issues in axe doesn't mean perfect a11y
-	console.log(colors.italic(
-		'Please note that only 20% to 50% of all accessibility ' +
-		'issues can automatically be detected. \nManual testing is ' +
-		'always required. For more information see:\n%s\n'
-	), link(
-		'https://dequeuniversity.com/curriculum/courses/testingmethods'
-	));
-}).catch((e) => {
-	console.log(' ');
-	if (!program['show-errors']) {
-		console.log(error('An error occurred while testing this page.'));
-	} else {
+		}
+	})
+	.then(() => {
+		// Give a notification that 0 issues in axe doesn't mean perfect a11y
 		console.log(
-			error('Error: %j \n $s'),
-			e.message,
-			e.stack
+			colors.italic(
+				'Please note that only 20% to 50% of all accessibility ' +
+					'issues can automatically be detected. \nManual testing is ' +
+					'always required. For more information see:\n%s\n'
+			),
+			link('https://dequeuniversity.com/curriculum/courses/testingmethods')
 		);
-	}
+	})
+	.catch(e => {
+		console.log(' ');
+		if (!program['show-errors']) {
+			console.log(error('An error occurred while testing this page.'));
+		} else {
+			console.log(error('Error: %j \n $s'), e.message, e.stack);
+		}
 
-	console.log('Please report the problem to: ' +
-		link('https://github.com/dequelabs/axe-cli/issues/') + '\n');
-	process.exit(1);
-});
+		console.log(
+			'Please report the problem to: ' +
+				link('https://github.com/dequelabs/axe-cli/issues/') +
+				'\n'
+		);
+		process.exit(1);
+	});
--- test/webdriver.js
+++ test/webdriver.js
@@ -1,25 +1,27 @@
 /* global mocha */
 'use strict';
 
-const assert = require('chai').assert
-const {startDriver, stopDriver} = require('../lib/webdriver')
-const chromedriver = require('chromedriver')
-const chrome = require('selenium-webdriver/chrome')
+const assert = require('chai').assert;
+const { startDriver, stopDriver } = require('../lib/webdriver');
+const chromedriver = require('chromedriver');
+const chrome = require('selenium-webdriver/chrome');
 
 describe('startDriver', () => {
-	let config, browser
+	let config, browser;
 	beforeEach(() => {
 		browser = 'chrome-headless';
 		config = {
-			get browser() { return browser }
-		}
-	})
+			get browser() {
+				return browser;
+			}
+		};
+	});
 
 	afterEach(async () => {
-		stopDriver(config)
-		const service = chrome.getDefaultService()
+		stopDriver(config);
+		const service = chrome.getDefaultService();
 		if (service.isRunning()) {
-			await service.stop()
+			await service.stop();
 
 			// An unfortunately hacky way to clean up
 			// the service. Stop will shut it down,
@@ -27,59 +29,59 @@ describe('startDriver', () => {
 			service.address_ = null;
 			chrome.setDefaultService(null);
 		}
-	})
+	});
 
 	it('creates a driver', async () => {
-		await startDriver(config)
+		await startDriver(config);
 
-		assert.isObject(config.driver)
-		assert.isFunction(config.driver.manage)
-	})
+		assert.isObject(config.driver);
+		assert.isFunction(config.driver.manage);
+	});
 
 	xit('sets the config.browser as the browser', done => {
-		browser = 'chrome'
+		browser = 'chrome';
 		startDriver(config)
 			.then(config => config.driver.getCapabilities())
 			.then(capabilities => {
-				assert.equal(capabilities.get('browserName'), browser)
+				assert.equal(capabilities.get('browserName'), browser);
 			})
-			.then(done, done)
-	})
+			.then(done, done);
+	});
 
 	it('sets the browser as chrome with chrome-headless', async () => {
-		browser = 'chrome-headless'
-		await startDriver(config)
-		const capabilities = await config.driver.getCapabilities()
+		browser = 'chrome-headless';
+		await startDriver(config);
+		const capabilities = await config.driver.getCapabilities();
 
-		assert.equal(capabilities.get('browserName'), 'chrome')
-	})
+		assert.equal(capabilities.get('browserName'), 'chrome');
+	});
 
 	it('uses the chromedriver path with chrome-headless', async () => {
-		browser = 'chrome-headless'
-		await startDriver(config)
-		const service = chrome.getDefaultService()
+		browser = 'chrome-headless';
+		await startDriver(config);
+		const service = chrome.getDefaultService();
 
-		assert.equal(service.executable_, chromedriver.path)
-	})
+		assert.equal(service.executable_, chromedriver.path);
+	});
 
 	it('sets the --headless flag with chrome-headless', async () => {
-		browser = 'chrome-headless'
-		const { builder } = await startDriver(config)
-		const capabilities = await builder.getCapabilities()
+		browser = 'chrome-headless';
+		const { builder } = await startDriver(config);
+		const capabilities = await builder.getCapabilities();
 		const chromeOptions = capabilities.get('chromeOptions');
 
-		assert.isObject(chromeOptions)
-		assert.deepEqual(chromeOptions.args, ['--headless'])
-	})
-})
+		assert.isObject(chromeOptions);
+		assert.deepEqual(chromeOptions.args, ['--headless']);
+	});
+});
 
 describe('stopDriver', () => {
 	it('calls browser.quit', () => {
 		let called = 0;
 		stopDriver({
-				browser: 'chrome-headless',
-				driver: { quit: () => called++ }
-			})
-		assert.equal(called, 1)
-	})
-})
+			browser: 'chrome-headless',
+			driver: { quit: () => called++ }
+		});
+		assert.equal(called, 1);
+	});
+});
